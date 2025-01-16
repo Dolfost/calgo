57c9b368107f59b6581b672bdac30459d7be2836
Vladyslav Rehan <rehanvladyslav@gmail.com>
2025-01-16 14:34:52 +0200

    Add submatrix tests

---
+++ src/calgo/tests/submatrix.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <calgo/mat.hpp>
+
+int main(int argc, char** argv) {
+	ca::Mat<double> m = {
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15},
+		{16, 17, 18, 19, 20},
+		{21, 22, 23, 24, 25},
+	};
+
+	std::cout << "mat 5x5:\n" << m;
+	std::cout << "\nsub(0,0,5,5):\n" << m.mat(0,0, 5, 5);
+	std::cout << "\nsub(1,1,4,3):\n" << m.mat(1,1, 4, 3);
+
+
+	return 0;
+}
---
+++ src/calgo/tests/submatrix1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <calgo/mat.hpp>
+
+int main(int argc, char** argv) {
+	ca::Mat<double> m = {
+		{-2, 3, 0, 0, 0, 0},
+		{-1, 2, -1, 0, 0, 2},
+		{-5, 2, 0, 1, 0, 10},
+		{1, 2, 0, 0, -1, 4},
+	};
+
+	std::cout << "mat 4x6:\n" << m 
+		<< "\nmat(1, 0, r-1, c-1):\n" << m.mat(
+			1, 0, m.rows() - 1,
+			m.cols() - 1
+		);
+
+
+	return 0;
+}
